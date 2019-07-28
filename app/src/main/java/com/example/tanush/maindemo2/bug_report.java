e796d8b666c3ab4a162146b3da8000711e36d5f2
divyank <lunkaddivyank@gmail.com>
2019-07-29 01:14:09 +0530

    Bug report done!

--- app/src/main/java/com/example/tanush/maindemo2/bug_report.java
+++ app/src/main/java/com/example/tanush/maindemo2/bug_report.java
@@ -1,14 +1,38 @@
 package com.example.tanush.maindemo2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class bug_report extends AppCompatActivity {
-
+    CardView dev1, dev2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bug_report);
+
+        dev1 = (CardView) findViewById(R.id.dev1);
+        dev2 = (CardView) findViewById(R.id.dev2);
+
+        dev1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + "8059130863"));
+                startActivity(intent);
+            }
+        });
+        dev2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + "7507457622"));
+                startActivity(intent);
+            }
+        });
     }
 }
