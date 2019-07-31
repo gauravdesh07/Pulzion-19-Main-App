49a939645cda07adfbab83ebb2b125ff739095db
gauravdesh26071999 <gauravdesh26@gmail.com>
2019-07-31 22:50:18 +0530

    Receipt Generation Updated(RecyclerView)

--- app/src/main/java/com/example/tanush/maindemo2/Workshops.java
+++ app/src/main/java/com/example/tanush/maindemo2/Workshops.java
@@ -1,4 +1,14 @@
 package com.example.tanush.maindemo2;
 
-public class Workshops {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Workshops extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workshops);
+    }
 }
