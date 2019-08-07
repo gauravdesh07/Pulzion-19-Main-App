0b35dd7e403e4ea7a4df287d2600071f4dd39d0e
tsm9999 <maddiwartanush@gmail.com>
2019-08-07 15:42:52 +0530

    Messaging Service activted

--- app/src/main/java/com/pasc/pulzion19/Events.java
+++ app/src/main/java/com/pasc/pulzion19/Events.java
@@ -61,6 +61,8 @@ public class Events extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
         toggle.syncState();
         navigationView.setItemIconTintList(null);
         navigationView.setNavigationItemSelectedListener(this);
